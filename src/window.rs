0521bd81fd835a19fafaf63ba626a17f9ef8f9a2
jeffshee <jeffshee8969@gmail.com>
2024-09-07 22:35:14 +0900

    add: cargo deps

--- src/window.rs
+++ src/window.rs
@@ -62,7 +62,7 @@ glib::wrapper! {
 }
 
 impl HotaruWindow {
-    pub fn new<P: glib::IsA<gtk::Application>>(application: &P) -> Self {
+    pub fn new<P: IsA<gtk::Application>>(application: &P) -> Self {
         glib::Object::builder()
             .property("application", application)
             .build()
