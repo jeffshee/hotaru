b728fe53bdcc7234468b40e1f4f9b96cdad512f0
jeffshee <jeffshee8969@gmail.com>
2024-09-26 15:34:32 +0900

    feat: initial flatpak

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "hotaru";
-pub static LOCALEDIR: &str = "/home/jeffshee/.local/share/locale";
-pub static PKGDATADIR: &str = "/home/jeffshee/.local/share/hotaru";
